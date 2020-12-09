6936a111594d599d0a9780f1e59c4921980642b9
xuluyuan <1095965664@qq.com>
2020-12-09 14:00:19 +0800

    登录页面完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/global.css'
+import axios from 'axios'
+import qs from 'qs'
+import {Button, Card, Col, Form, FormItem, Input, Message, Row, Tag} from 'element-ui';
+Vue.prototype.qs=qs
+Vue.prototype.axios=axios
+axios.defaults.baseURL="http://127.0.0.1:8888/api/private/v1/"
+
 
 Vue.config.productionTip = false
-
+Vue.use(Button)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Tag)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,48 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../views/Login.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:'/',
+    redirect:'/login'
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/home',
+    name: 'Home',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   }
 ]
 
+
 const router = new VueRouter({
   routes
+})
+
+//只有在登录情况下才可以访问其他的API
+//通过路由守卫进行登录判断
+router.beforeEach((to,from,next)=>{
+  //to 将要访问的页面
+  //from  来源页面
+  //next 是一个函数，表示放行 next()---放行  next('/login')----强行跳转到login页面
+  if(to.path=="/login"){
+    next()
+  }
+  let tokenStr=sessionStorage.getItem('token')
+  if(tokenStr===null){
+    next('/login')
+  }else{
+    next()
+  }
 })
 
 export default router
